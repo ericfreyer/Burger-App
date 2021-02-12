8f6e5074fcbddfef77c46a9e7fc760d9be30aba4
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-02-12 15:54:19 -0500

    set up get request in controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,30 @@
 const express = require('express');
-
 const router = express.Router();
+const burger = require('../models/burger.js');
 
-const cat = require('../models/burger.js');
+router.get('/', (req, res) => {
+    cat.all((data) => {
+      const hbsObject = {
+        burgers: data,
+      };
+      console.log(hbsObject);
+      res.render('index', hbsObject);
+    });
+});
+
+router.post('/api/burgers', (req, res) => {
+    cat.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+
+  router.put("/api/burgers/:id", function(req, res) {
+    var id = req.params.id;
+    // console.log(id);
+    burger.updateOne("devoured", true, id, function(results){
+        res.json(results);
+    });
+});
+
+module.exports = router;
