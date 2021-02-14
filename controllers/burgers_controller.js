74ae77df0c1dbf4cedc0cda39464bfaa5df026c2
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-02-14 15:48:38 -0500

    fixed partials handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -19,9 +19,26 @@ router.post("/burgers/create", function (req, res) {
   });
 });
 
-router.post('/burger/eat/:id', function (req, res) {
-  burger.updateOne(req.params.id, function() {
-    res.redirect('/index');
-  });
+router.put('/api/burgers/:id', (req, res) => {
+  const condition = `id = ${req.params.id}`;
+
+  console.log('condition', condition);
+
+  burger.update(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
 });
+
+
+
 module.exports = router;
