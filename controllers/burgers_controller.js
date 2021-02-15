fb1b21bcd22d0da0fde75cf829fde20961ea121c
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-02-15 12:56:52 -0500

    trying to debug

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const burger = require('../models/burger.js');
 
 router.get('/', (req, res) => {
-    burger.selectAll((data) => {
+    burger.all((data) => {
       const hbsObject = {
         burgers: data,
       };
@@ -12,8 +12,8 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/api/cats', (req, res) => {
-  cat.create(['name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
+router.post('/api/burgers', (req, res) => {
+  cat.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES('Double Cheese Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES('Veggie Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES('Portobello Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portobello Burger', false);
