76ba48ea6094984ce2b2c53c2444193dd789101c
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-02-15 11:27:45 -0500

    fixing orm.js

--- config/orm.js
+++ config/orm.js
@@ -2,26 +2,63 @@
 var connection = require('./connection.js');
 
 var orm = {
-	selectAll: function( tablename,cb){
-		connection.query('SELECT * FROM ??;', [tablename], function(err, result){
+	selectAll: function(tableInput,cb){
+		connection.query(`SELECT * FROM ??; ${tableInput};`, function(err, result){
 			if (err) throw err;
 			// console.log(result);
 			 cb(result);
-		})
-	},
-	insertOne: function (tablename, colname, burger_name, cb) {
-		connection.query("INSERT INTO ?? (??) VALUES (?)", [ tablename ,colname, burger_name], function (err, result) {
-			if (err) throw err;			
-			 cb(result);
 		});
 	},
-	updateOne: function ( tablename,colname, devoured, id, cb) {		
-		connection.query("UPDATE ?? SET ?? = ? WHERE id = ?", [tablename , colname ,devoured , id], function (err, result) {
-			if (err) throw err;			
-			cb(result);
-			
+	insertOne: function (table, cols, vals, cb) {
+		let queryString = `INSERT INTO ${table}`;
+	
+		queryString += ' (';
+		queryString += cols.toString();
+		queryString += ') ';
+		queryString += 'VALUES (';
+		queryString += printQuestionMarks(vals.length);
+		queryString += ') ';
+	
+		console.log(queryString);
+	
+		connection.query(queryString, vals, (err, result) => {
+		  if (err) {
+			throw err;
+		  }
+	
+		  cb(result);
 		});
-	},
-};
+	  },
+	updateOne: function (table, objColVals, condition, cb) {
+		let queryString = `UPDATE ${table}`;
+	
+		queryString += ' SET ';
+		queryString += objToSql(objColVals);
+		queryString += ' WHERE ';
+		queryString += condition;
+	
+		console.log(queryString);
+		connection.query(queryString, (err, result) => {
+		  if (err) {
+			throw err;
+		  }
+	
+		  cb(result);
+		});
+	  },
+	  delete(table, condition, cb) {
+		let queryString = `DELETE FROM ${table}`;
+		queryString += ' WHERE ';
+		queryString += condition;
+	
+		connection.query(queryString, (err, result) => {
+		  if (err) {
+			throw err;
+		  }
+	
+		  cb(result);
+		});
+	  },
+	};
 
 module.exports = orm;
