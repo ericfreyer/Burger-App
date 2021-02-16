6f6359c9de76f135a2998744b3d117c112635e50
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-02-16 14:34:05 -0500

    trying to deploy

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const password = require("../.env")
 
-const connection;
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
